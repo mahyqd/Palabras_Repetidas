54334631237957ead77660592b2851fa77032bda
Mahy Quintana Díaz <mahy_1990@hotmail.com>
2013-05-18 12:46:25 -0400

    Test2 compareWords

--- TestPalabrasRepetidas.java
+++ TestPalabrasRepetidas.java
@@ -2,6 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
 import palabrasrepetidas.PalabrasRepetidas;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -15,19 +16,25 @@ import static org.junit.Assert.*;
  * @author Administrador
  */
 public class TestPalabrasRepetidas {
-    
+
     public TestPalabrasRepetidas() {
     }
-    
-   
+
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
     @Test
-    public void compareWordsTest(){
+    public void compareWordsTest() {
         boolean iguales = false;
-        iguales = PalabrasRepetidas.compareWords("Hola","Hola");
+        iguales = PalabrasRepetidas.compareWords("Hola", "Hola");
         assertEquals(true, iguales);
-    
     }
+    
+    @Test
+    public void compareWordsTest2() {
+        boolean iguales = false;
+        iguales = PalabrasRepetidas.compareWords("Hola", "adios");
+        assertEquals(false, iguales);
+    }
+
 }
